81b33e24745705775bb3664d5c0d9262f549b0ce
Clément COLIN <clement.colin69@gmail.com>
2022-12-06 11:40:20 +0100

    delete child and fix extent

--- routes/collectionsRouting.js
+++ routes/collectionsRouting.js
@@ -5,7 +5,7 @@ const pathToCollections = "./assets/collections.json";
 
 const CollectionsHandler = require("../src/collectionsHandler");
 
-const collections = new CollectionsHandler(pathToCollections);
+var collections = new CollectionsHandler(pathToCollections);
 
 let router = express.Router();
 
